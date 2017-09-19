544fdf18d0c4f247f39942b4876c94238c73cc2f
jsamol <julia.samol@gmail.com>
2017-09-19 17:40:12 +0200

    Add simple routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './root/Home';
+import Login from './auth/Login';
 
 import './App.css';
+
+function PrivateRoute({ component: Component, ...rest}) {
+  return (
+    <Route {...rest} render={props => (
+      <Component {...props}/>
+    )}/>
+  );
+}
+
+PrivateRoute.propTypes = {
+  path: PropTypes.string.isRequired,
+  component: PropTypes.object.isRequired
+};
 
 class App extends Component {
   render() {
     return (
-      <h1>Home</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login}/>
+          <PrivateRoute exact path="/" component={Home}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
