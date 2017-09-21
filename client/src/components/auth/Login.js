cfcac77c841569d9e1ff71dbe647d417553bf952
jsamol <julia.samol@gmail.com>
2017-09-21 15:28:16 +0200

    Handle login button click

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Button, Form, FormControl, FormGroup, Glyphicon, InputGroup } from 'react-bootstrap';
 
 import './Login.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleLogin() {
+    axios.post("/auth", {
+      username: this.state.username,
+      password: this.state.password
+    }).then(function(res) {
+
+    }).catch(function(error) {
+      console.error(error);
+    });
+  }
+
+  handleInputChange(event) {
+    let target = event.target;
+    let key = target.id;
+    let value = target.value;
+
+    let newState = this.state;
+    newState[key] = value;
+
+    this.setState(newState);
+  }
+
   render() {
     return (
       <div id="content">
         <Glyphicon id="cloud" glyph="cloud"/>
-        <Form className="login-form">
+        <Form className="login-form" onSubmit={this.handleLogin}>
           <FormGroup>
             <InputGroup>
               <InputGroup.Addon>
                 <Glyphicon glyph="user"/>
               </InputGroup.Addon>
-              <FormControl type="text"/>
+              <FormControl id="username" type="text" onChange={this.handleInputChange}/>
             </InputGroup>
           </FormGroup>
           <FormGroup>
@@ -22,10 +57,10 @@ class Login extends Component {
               <InputGroup.Addon>
                 <Glyphicon glyph="lock"/>
               </InputGroup.Addon>
-              <FormControl type="password"/>
+              <FormControl id="password" type="password" onChange={this.handleInputChange}/>
             </InputGroup>
           </FormGroup>
-          <Button type="cancel">Cancel</Button>
+          <Button type="reset">Cancel</Button>
           <Button className="login-button" type="submit">Login</Button>
         </Form>
       </div>
